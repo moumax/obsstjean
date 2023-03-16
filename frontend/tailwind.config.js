3c9b07a0b2f5dc155c05f51a3ffc8c89a1df1c4a
Marc LANTOL <marc.lantol@gmail.com>
2023-03-16 23:51:39 +0100

    Init tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      xs: "0px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+      "3xl": "1600px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
