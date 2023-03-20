b741a2926a81a0bccf1c77cd68098ab408ac1c79
Marc LANTOL <marc.lantol@gmail.com>
2023-03-20 19:01:30 +0100

    Fix router and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,19 @@
 /* eslint-disable import/no-unresolved */
+import { Routes, Route } from "react-router-dom";
+import Administration from "@pages/Administration/Administration";
 
 import EnterPoint from "@components/enter_point/enter_point";
-import Observatoire from "@components/observatoire/observatoire";
-import About from "@components/about/about";
+
 import "./App.css";
-import Footer from "@components/footer/footer";
-import Contact from "@components/contact/contact";
-import Events from "@components/events/events";
 
 function App() {
   return (
     <main className="w-full h-full bg-[#072449] overflow-hidden">
       <div className="flex flex-col items-center">
-        <EnterPoint />
-        <Observatoire />
-        <About />
-        <Events />
-        <Contact />
-        <Footer />
+        <Routes>
+          <Route path="/" element={<EnterPoint />} />
+          <Route path="Administration" element={<Administration />} />
+        </Routes>
       </div>
     </main>
   );
---
+++ frontend/src/pages/Administration/Administration.jsx
@@ -0,0 +1,7 @@
+export default function Administration() {
+  return (
+    <section>
+      <h2>Administration</h2>
+    </section>
+  );
+}
