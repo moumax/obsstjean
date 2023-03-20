173db171b9e7f6bd3e1865deffee5206180d16ed
Marc LANTOL <marc.lantol@gmail.com>
2023-03-20 18:36:00 +0100

    List of events are displayed

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    origin: process.env.FRONTEND_URL ?? "http://localhost:5173",
+    credentials: true,
     optionsSuccessStatus: 200,
   })
 );
--- frontend/src/components/events/cardEvent.jsx
+++ frontend/src/components/events/cardEvent.jsx
@@ -1,9 +1,13 @@
-const cardEvent = ({ data }) => {
+const cardEvent = (event) => {
+  // console.log(data)
   return (
-    <div className="w-96 flex flex-col items-center bg-red-200">
-      <img className="w-fit rounded-xl" src={data.image} alt={data.alt} />
-      <h3>{data.title}</h3>
-      <p className="w-fit text-center">{data.textFr}</p>
+    <div className="w-96 flex flex-col items-center ">
+      <div className="w-[90vw] border-solid border-2 border-slate-300 p-3 rounded-xl mb-5">
+        <h3>{event.data.title}</h3>
+        <p>{event.data.description}</p>
+        <p>{event.data.date}</p>
+        <p>{event.data.site}</p>
+      </div>
     </div>
   );
 };
