532996e2b5577ed4090d934b78cd53d32b776b15
Marc LANTOL <marc.lantol@gmail.com>
2023-03-24 10:04:47 +0100

    Style about and about card

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -6,9 +6,9 @@ import datasAbout from "/src/datas/datasAbout.js";
 
 export default function About() {
   return (
-    <section className="w-[90vw] mt-10 flex flex-col items-center bg-slate-400">
-      <h2>A propos</h2>
-      <div className="flex flex-wrap justify-center">
+    <section className="w-[90vw] mt-10 flex flex-col items-center">
+      <h2 className="font-exo2 text-xl text-white pb-4">Nos activités</h2>
+      <div className="flex flex-wrap justify-center w-full">
         {datasAbout.map((data) => (
           <CardAbout data={data} key={data.id} />
         ))}
--- frontend/src/components/about/cardAbout.jsx
+++ frontend/src/components/about/cardAbout.jsx
@@ -1,9 +1,13 @@
 const cardAbout = ({ data }) => {
   return (
-    <div className="w-96 flex flex-col items-center bg-red-200">
-      <img className="w-fit rounded-xl" src={data.image} alt={data.alt} />
-      <h3>{data.title}</h3>
-      <p className="w-fit text-center">{data.textFr}</p>
+    <div className="w-96 flex flex-col items-center pt-5 font-exo2">
+      <img className="w-fit rounded-xl pb-2" src={data.image} alt={data.alt} />
+      <h3 className="text-white text-xs italic opacity-70 pb-2">
+        {data.title}
+      </h3>
+      <p className="w-fit text-center text-white text-xs opacity-40">
+        {data.textFr}
+      </p>
     </div>
   );
 };
