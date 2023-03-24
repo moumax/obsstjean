6a0bba2528aceb0905a6aedabdb937ea437e0a1f
Marc LANTOL <marc.lantol@gmail.com>
2023-03-24 12:28:27 +0100

    Style events

--- frontend/src/components/events/cardEvent.jsx
+++ frontend/src/components/events/cardEvent.jsx
@@ -2,11 +2,13 @@ const cardEvent = (event) => {
   // console.log(data)
   return (
     <div className="w-96 flex flex-col items-center ">
-      <div className="w-[90vw] border-solid border-2 border-slate-300 p-3 rounded-xl mb-5">
-        <h3>{event.data.title}</h3>
-        <p>{event.data.description}</p>
-        <p>{event.data.date}</p>
-        <p>{event.data.site}</p>
+      <div className="w-[90vw] border-solid border-2 border-yellow-300 p-3 rounded-xl mb-5">
+        <h3 className="text-white opacity-70">{event.data.title}</h3>
+        <p className="text-white opacity-50 text-xs">
+          {event.data.description}
+        </p>
+        <p className="text-white opacity-50 text-xs">{event.data.date}</p>
+        <p className="text-white opacity-70">{event.data.site}</p>
       </div>
     </div>
   );
