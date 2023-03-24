eef2b6f0f0da9668cd6e4ff3e4a29bac01c164e8
Marc LANTOL <marc.lantol@gmail.com>
2023-03-24 08:32:57 +0100

    Switch browser router to main Add a context to block routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
 /* eslint-disable import/no-unresolved */
-import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Administration from "@pages/Administration/Administration";
 import EnterPoint from "@components/enter_point/enter_point";
 import Login from "@pages/Administration/Login";
-
-import { CurrentUserContextProvider } from "./contexts/userContext";
+import CurrentUserContext from "./contexts/userContext";
 
 import "./App.css";
 
 function App() {
+  const { user } = useContext(CurrentUserContext);
   return (
-    <CurrentUserContextProvider>
-      <main className="w-full h-full bg-[#072449] overflow-hidden">
-        <div className="flex flex-col items-center">
-          <Routes>
-            <Route path="/" element={<EnterPoint />} />
-            <Route path="login" element={<Login />} />
-            <Route path="Administration" element={<Administration />} />
-          </Routes>
-        </div>
-      </main>
-    </CurrentUserContextProvider>
+    <main className="w-full h-full bg-[#072449] overflow-hidden">
+      <div className="flex flex-col items-center">
+        <Routes>
+          <Route path="/" element={<EnterPoint />} />
+          <Route path="login" element={<Login />} />
+          <Route
+            path="Administration"
+            element={user ? <Administration /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </div>
+    </main>
   );
 }
 
