b384f6a66e41e817ab4772fd0050ecd0685e71a8
Marc LANTOL <marc.lantol@gmail.com>
2023-04-20 19:32:23 +0200

    Improve Button rules

--- frontend/src/components/assets/Button.jsx
+++ frontend/src/components/assets/Button.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-function Button({ type = "button", bgprimary, textprimary, label, onClick }) {
+function Button({
+  type = "button",
+  bgprimary,
+  textprimary,
+  label,
+  onClick,
+  height,
+}) {
   return (
     <button
       // eslint-disable-next-line react/button-has-type
       type={type}
       onClick={(e) => onClick(e)}
-      className={`${bgprimary} py-1 px-3 rounded-lg text-white text-sm font-exo2 hover:${textprimary} cursor-pointer duration-300`}
+      className={`${bgprimary} py-1 px-3 rounded-lg text-white text-sm font-exo2 hover:${textprimary} cursor-pointer duration-300 ${height}`}
     >
       {label}
     </button>
