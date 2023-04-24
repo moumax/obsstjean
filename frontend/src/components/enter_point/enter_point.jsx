0291bd5b0786ef3455a1e2f4ba8886b5e06500c6
Marc LANTOL <marc.lantol@gmail.com>
2023-04-24 13:27:14 +0200

    Include buttons on footer

--- frontend/src/components/enter_point/enter_point.jsx
+++ frontend/src/components/enter_point/enter_point.jsx
@@ -1,5 +1,3 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 /* eslint-disable import/no-unresolved */
 import About from "@components/about/about";
 import Weather from "@components/weather/Weather";
@@ -8,26 +6,8 @@ import Observatoire from "@components/observatoire/observatoire";
 import Contact from "@components/contact/contact";
 import Events from "@components/enter_point/events";
 import Header from "@components/header/Header";
-import { toast } from "react-toastify";
-import axiosAPI from "../../services/axiosAPI";
-import CurrentUserContext from "../../contexts/userContext";
-import Button from "../assets/Button";
 
 export default function EnterPoint() {
-  const { user, setUser } = useContext(CurrentUserContext);
-  const navigate = useNavigate();
-
-  const handleDisconnect = () => {
-    axiosAPI
-      .get("http://localhost:5000/api/auth/logout")
-      .then(() => {
-        localStorage.clear();
-        setUser(undefined);
-        toast.warning("Tu es déconnecté !");
-      })
-      .catch((err) => console.error(err.message));
-  };
-
   return (
     <>
       <Header />
@@ -37,31 +17,6 @@ export default function EnterPoint() {
       <Events />
       <Contact />
       <Footer />
-      <div className="flex gap-2 absolute top-2 right-2">
-        {user && (
-          <Button
-            label="Administration"
-            bgprimary="bg-blue-600"
-            onClick={() => navigate("/administration")}
-          />
-        )}
-        {user && (
-          <Button
-            label="Logout"
-            bgprimary="bg-red-600"
-            onClick={() => {
-              handleDisconnect();
-            }}
-          />
-        )}
-        {!user && (
-          <Button
-            label="Login"
-            bgprimary="bg-blue-600"
-            onClick={() => navigate("/login")}
-          />
-        )}
-      </div>
     </>
   );
 }
--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,9 +1,61 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Button from "../assets/Button";
+import CurrentUserContext from "../../contexts/userContext";
+import axiosAPI from "../../services/axiosAPI";
+
 export default function Footer() {
+  const { user, setUser } = useContext(CurrentUserContext);
+  const navigate = useNavigate();
+
+  const handleDisconnect = () => {
+    axiosAPI
+      .get("http://localhost:5000/api/auth/logout")
+      .then(() => {
+        localStorage.clear();
+        setUser(undefined);
+        toast.warning("Tu es déconnecté !");
+      })
+      .catch((err) => toast.error(err.message));
+  };
+
   return (
-    <footer className="pt-5">
+    <footer className="flex flex-col pt-5">
       <div className="flex flex-col items-center text-xs text-white opacity-40">
         <p>© 2019 - 2023 -- Marc LANTOL</p>
         <p>Observatoire Astronomique de Saint Jean Le Blanc (V3.0.0)</p>
+      </div>
+      <div className="flex justify-end gap-2 p-5 opacity-30">
+        {user && (
+          <Button
+            label="Administration"
+            bgprimary="bg-blue-600"
+            height="h-8"
+            text="text-xs"
+            onClick={() => navigate("/administration")}
+          />
+        )}
+        {user && (
+          <Button
+            label="Logout"
+            bgprimary="bg-red-600"
+            height="h-8"
+            text="text-xs"
+            onClick={() => {
+              handleDisconnect();
+            }}
+          />
+        )}
+        {!user && (
+          <Button
+            label="Login"
+            bgprimary="bg-blue-600"
+            height="h-8"
+            text="text-xs"
+            onClick={() => navigate("/login")}
+          />
+        )}
       </div>
     </footer>
   );
