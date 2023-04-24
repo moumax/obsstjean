c61e7d6d53b7ef6e0fc2e89833fbc0561ef31803
Marc LANTOL <marc.lantol@gmail.com>
2023-04-24 13:07:14 +0200

    Create header component

--- frontend/src/components/enter_point/enter_point.jsx
+++ frontend/src/components/enter_point/enter_point.jsx
@@ -7,13 +7,11 @@ import Footer from "@components/footer/footer";
 import Observatoire from "@components/observatoire/observatoire";
 import Contact from "@components/contact/contact";
 import Events from "@components/enter_point/events";
+import Header from "@components/header/Header";
 import { toast } from "react-toastify";
 import axiosAPI from "../../services/axiosAPI";
 import CurrentUserContext from "../../contexts/userContext";
 import Button from "../assets/Button";
-
-import Image from "../../assets/logo/logo.png";
-import EnterPointImage from "../../assets/enter_point/enter_point.jpg";
 
 export default function EnterPoint() {
   const { user, setUser } = useContext(CurrentUserContext);
@@ -32,58 +30,38 @@ export default function EnterPoint() {
 
   return (
     <>
-      <header className="w-[90vw] mt-10 flex flex-col items-center">
-        <div className="flex gap-2 absolute top-2 right-2">
-          {user && (
-            <Button
-              label="Administration"
-              bgprimary="bg-blue-600"
-              onClick={() => navigate("/administration")}
-            />
-          )}
-          {user && (
-            <Button
-              label="Logout"
-              bgprimary="bg-red-600"
-              onClick={() => {
-                handleDisconnect();
-              }}
-            />
-          )}
-          {!user && (
-            <Button
-              label="Login"
-              bgprimary="bg-blue-600"
-              onClick={() => navigate("/login")}
-            />
-          )}
-        </div>
-
-        <div className="flex flex-col items-center mt-10 text-center">
-          <img
-            className="w-[50vw]"
-            src={Image}
-            alt="logo observatoire de saint jean le blanc"
-          />
-          <h1 className="text-xl font-exo2 text-transparent bg-clip-text bg-gradient-to-b from-[#fffc08] to-[#575506] pt-3">
-            Observatoire de Saint Jean Le Blanc
-          </h1>
-          <h2 className="text-sm  font-exo2 text-white opacity-50 break-keep pt-1">
-            Association loi 1901 pour la promotion de l'astronomie amateur
-          </h2>
-          <img
-            className="my-5 rounded-xl"
-            src={EnterPointImage}
-            alt="observatoire"
-          />
-        </div>
-      </header>
+      <Header />
       <Weather />
       <Observatoire />
       <About />
       <Events />
       <Contact />
       <Footer />
+      <div className="flex gap-2 absolute top-2 right-2">
+        {user && (
+          <Button
+            label="Administration"
+            bgprimary="bg-blue-600"
+            onClick={() => navigate("/administration")}
+          />
+        )}
+        {user && (
+          <Button
+            label="Logout"
+            bgprimary="bg-red-600"
+            onClick={() => {
+              handleDisconnect();
+            }}
+          />
+        )}
+        {!user && (
+          <Button
+            label="Login"
+            bgprimary="bg-blue-600"
+            onClick={() => navigate("/login")}
+          />
+        )}
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import Image from "../../assets/logo/logo.png";
+import EnterPointImage from "../../assets/enter_point/enter_point.jpg";
+
+export default function Header() {
+  return (
+    <header className="w-[90vw] mt-10 flex flex-col items-center">
+      <div className="flex flex-col items-center mt-10 text-center">
+        <img
+          className="w-[50vw]"
+          src={Image}
+          alt="logo observatoire de saint jean le blanc"
+        />
+        <h1 className="text-xl font-exo2 text-transparent bg-clip-text bg-gradient-to-b from-[#fffc08] to-[#575506] pt-3">
+          Observatoire de Saint Jean Le Blanc
+        </h1>
+        <h2 className="text-sm  font-exo2 text-white opacity-50 break-keep pt-1">
+          Association loi 1901 pour la promotion de l'astronomie amateur
+        </h2>
+        <img
+          className="my-5 rounded-xl"
+          src={EnterPointImage}
+          alt="observatoire"
+        />
+      </div>
+    </header>
+  );
+}
