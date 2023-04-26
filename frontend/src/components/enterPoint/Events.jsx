8d5b531b08e77b178e8ee1942bfcf6c6f91f1c46
Marc LANTOL <marc.lantol@gmail.com>
2023-04-26 12:47:18 +0200

    Refacto events

--- frontend/src/components/enterPoint/Events.jsx
+++ frontend/src/components/enterPoint/Events.jsx
@@ -3,37 +3,18 @@ import { useState, useEffect } from "react";
 // import useSWR, { useSWRConfig } from "swr";
 import axiosAPI from "../../services/axiosAPI";
 import CardEvent from "../administration/CardEvent";
+import sortedByDate from "../../utils/date";
 
 export default function EventsList() {
   const [events, setEvents] = useState([]);
 
   const baseURLevents = "http://localhost:5000/api/events";
 
-  function sorterByDate(tab) {
-    const now = new Date();
-    const sortedTab = tab.sort((a, b) => {
-      const dateA = new Date(a.date);
-      const dateB = new Date(b.date);
-
-      if (dateA < now && dateB < now) {
-        return dateA - dateB;
-      }
-      if (dateA < now) {
-        return 1;
-      }
-      if (dateB < now) {
-        return -1;
-      }
-      return dateA - dateB;
-    });
-    return sortedTab;
-  }
-
   useEffect(() => {
     axiosAPI
       .get(baseURLevents)
       .then((response) => {
-        setEvents(sorterByDate(response.data));
+        setEvents(sortedByDate(response.data));
       })
       .catch((error) => {
         console.error(error);
