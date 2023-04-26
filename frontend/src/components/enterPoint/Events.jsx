0e8300a2949879f1b677f3f1ca9e1fe700658673
Marc LANTOL <marc.lantol@gmail.com>
2023-04-26 14:37:30 +0200

    Reverse modifications

--- frontend/src/components/enterPoint/Events.jsx
+++ frontend/src/components/enterPoint/Events.jsx
@@ -1,32 +1,37 @@
 // eslint-disable-next-line no-unused-vars
-import { useState, useEffect } from "react";
-// import useSWR, { useSWRConfig } from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import axiosAPI from "../../services/axiosAPI";
 import CardEvent from "../administration/CardEvent";
-import sortedByDate from "../../utils/date";
 
 export default function EventsList() {
-  const [events, setEvents] = useState([]);
-
-  const baseURLevents = "http://localhost:5000/api/events";
-
-  useEffect(() => {
-    axiosAPI
-      .get(baseURLevents)
-      .then((response) => {
-        setEvents(sortedByDate(response.data));
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  const fetcher = async () => {
+    const response = await axiosAPI.get("http://localhost:5000/api/events");
+    const now = new Date();
+    const sortedEvents = response.data.sort((a, b) => {
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+      if (dateA < now && dateB < now) {
+        return dateA - dateB;
+      }
+      if (dateA < now) {
+        return 1;
+      }
+      if (dateB < now) {
+        return -1;
+      }
+      return dateA - dateB;
+    });
+    return sortedEvents;
+  };
+  const { data } = useSWR("events", fetcher);
+  if (!data) return <h2>Loading...</h2>;
 
   return (
     <section className="w-[90vw] mt-10 flex flex-col items-center">
       <h2 className="font-exo2 text-xl text-white pb-4">
         Calendrier des évènements
       </h2>
-      {events.map((event) => (
+      {data.map((event) => (
         <div key={event.id}>
           <CardEvent data={event} />
         </div>
