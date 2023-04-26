2b792723bb2030942a26c6b0a75340e106606128
Marc LANTOL <marc.lantol@gmail.com>
2023-04-26 19:34:49 +0200

    Fix create user swr

--- frontend/src/api/fetcher.js
+++ frontend/src/api/fetcher.js
@@ -1,7 +1,5 @@
 import axiosAPI from "../services/axiosAPI";
 
-function fetcher(url) {
+export default function fetcher(url) {
   return axiosAPI.get(url).then((res) => res.data);
 }
-
-export default fetcher;
