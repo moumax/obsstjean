bfd14adb774f942c37db775efac751d3a157f54c
Marc LANTOL <marc.lantol@gmail.com>
2023-04-26 15:02:13 +0200

    Create spinner and refacto fetch

--- frontend/src/components/enterPoint/Events.jsx
+++ frontend/src/components/enterPoint/Events.jsx
@@ -1,30 +1,28 @@
 // eslint-disable-next-line no-unused-vars
-import useSWR, { useSWRConfig } from "swr";
+import MoonLoader from "react-spinners/MoonLoader";
+import useSWR from "swr";
 import axiosAPI from "../../services/axiosAPI";
 import CardEvent from "../administration/CardEvent";
+import sortedByDate from "../../utils/date";
 
 export default function EventsList() {
-  const fetcher = async () => {
+  const getEvents = async () => {
     const response = await axiosAPI.get("http://localhost:5000/api/events");
-    const now = new Date();
-    const sortedEvents = response.data.sort((a, b) => {
-      const dateA = new Date(a.date);
-      const dateB = new Date(b.date);
-      if (dateA < now && dateB < now) {
-        return dateA - dateB;
-      }
-      if (dateA < now) {
-        return 1;
-      }
-      if (dateB < now) {
-        return -1;
-      }
-      return dateA - dateB;
-    });
-    return sortedEvents;
+    return sortedByDate(response.data);
   };
-  const { data } = useSWR("events", fetcher);
-  if (!data) return <h2>Loading...</h2>;
+
+  const { data } = useSWR("events", getEvents);
+  if (!data)
+    return (
+      <h2>
+        <MoonLoader
+          color="#36d7b7"
+          size={60}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </h2>
+    );
 
   return (
     <section className="w-[90vw] mt-10 flex flex-col items-center">
